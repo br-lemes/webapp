b2f362d85dffc370cf81af162fc61c5a380ff408
Breno Ramalho Lemes <breno@br-lemes.net>
2019-02-17 17:30:29 -0400

    Retrieve numbers data

---
+++ www/numbers/data/int/index.lua
@@ -0,0 +1,20 @@
+
+local webapp = require("pkg.webapp")
+
+local default = {
+	number  = 0,
+	count   = 3,
+	ndays   = tonumber(os.date("%H")) >= 10 and 21 or 20,
+	digits  = 3,
+	xoffset = 40,
+	yoffset = 15,
+}
+
+if mg.request_info.request_method == "GET" then
+	local data = webapp.read("data/numint.json")
+	data = webapp.default(data, default)
+	data = webapp.encode(data)
+	webapp.send(data ~= nil, data)
+else
+	webapp.send_notallowed()
+end
