5f0312118f52ac4c5b718a2bfaf04c9e783bb14d
Breno Ramalho Lemes <breno@br-lemes.net>
2019-02-17 22:25:50 -0400

    Last-Modified now consider all app modification time

--- www/index.lua
+++ www/index.lua
@@ -3,15 +3,7 @@ package.loaded.lfs = lfs
 local json = require("json")
 local mustache = require("mustache")
 
-mg.write("HTTP/1.0 200 OK\r\n")
-mg.write("Date:" .. os.date("! %a, %d %b %Y %H:%M:%S GMT") .. "\r\n")
-mg.write("Cache-Control: max-age=3600\r\n")
-mg.write("Last-Modified:" .. os.date("! %a, %d %b %Y %H:%M:%S GMT",
-	lfs.attributes("www/index.lua", "modification")) .. "\r\n")
-mg.write("Content-Type: text/html; charset=UTF-8\r\n")
-mg.write("Connection: close\r\n")
-mg.write("\r\n")
-
+local mtime = lfs.attributes("www/index.lua", "modification")
 local view = { }
 view.applist = { }
 
@@ -19,17 +11,30 @@ for file in lfs.dir("www") do
 	if file ~= "." and file ~= ".." then
 		local mode = lfs.attributes("www/" .. file, "mode")
 		if mode == "directory" then
-			local f, e = io.open(string.format("www/%s/info.json", file))
-			if f then
-				local buf = f:read("*a")
-				f:close()
-				local app = json.decode(buf)
-				app.href = file
-				table.insert(view.applist, app)
+			local n = string.format("www/%s/info.json", file)
+			local m = lfs.attributes(n, "modification")
+			if m then
+				if m > mtime then mtime = m end
+				local f, e = io.open(n)
+				if f then
+					local buf = f:read("*a")
+					f:close()
+					local app = json.decode(buf)
+					app.href = file
+					table.insert(view.applist, app)
+				end
 			end
 		end
 	end
 end
+
+mg.write("HTTP/1.0 200 OK\r\n")
+mg.write("Date:", os.date("! %a, %d %b %Y %H:%M:%S GMT"), "\r\n")
+mg.write("Cache-Control: max-age=3600\r\n")
+mg.write("Last-Modified:", os.date("! %a, %d %b %Y %H:%M:%S GMT", mtime), "\r\n")
+mg.write("Content-Type: text/html; charset=UTF-8\r\n")
+mg.write("Connection: close\r\n")
+mg.write("\r\n")
 
 local f, e = io.open("mustache/index")
 if not f then error(e) end
