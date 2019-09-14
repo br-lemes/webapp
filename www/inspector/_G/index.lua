17f4ad78eecdcb763540d76502d3f7b6d3a32927
Breno Ramalho Lemes <breno@br-lemes.net>
2019-09-14 03:06:08 -0400

    Change mode selection (html or snippet) and add json

--- www/inspector/_G/index.lua
+++ www/inspector/_G/index.lua
@@ -1,22 +1,14 @@
 
 package.loaded.lfs = lfs
 local mustache = require("mustache")
+local webapp = require("pkg.webapp")
 
 local view = { }
-
-mg.write("HTTP/1.0 200 OK\r\n")
-mg.write("Date:", os.date("! %a, %d %b %Y %H:%M:%S GMT"), "\r\n")
-mg.write("Cache-Control: no-cache\r\n")
-mg.write("Content-Type: text/html; charset=UTF-8\r\n")
-mg.write("Connection: close\r\n")
-mg.write("\r\n")
-
 view.path_info = string.format("/_G%s", mg.request_info.path_info or "")
 if view.path_info:sub(-1) == "/" then
 	view.path_info = view.path_info:sub(1, -2)
 end
 view.path_path = { }
-
 view.table = { }
 local full = ""
 local t = _G
@@ -49,7 +41,7 @@ end
 
 table.sort(view.table, sort)
 
-local template = [[
+local html = [[
 		<h1 class="section row">
 			{{#path_path}}
 			<a href="#" onclick="loadTable('/inspector{{{full}}}');">
@@ -77,13 +69,24 @@ local template = [[
 		</div>
 ]]
 
-local snippet = mg.get_var(mg.request_info.query_string, "snippet") == "true"
-if snippet then
-	mg.write(mustache.render(template, view))
-else
+local json = [[
+	{{#table}}
+		"{{{k}}}": "{{{value}}}",
+	{{/table}}
+]]
+
+local mode = { list = { default = "html", html = "html", json = "json", snippet = "snippet" } }
+mode.value = mg.get_var(mg.request_info.query_string, "mode")
+if not mode.list[mode.value] then mode.value = mode.list.default end
+
+if mode.value == "json" then
+	webapp.send_ok(string.format("{\n%s\n}", mustache.render(json, view):match("(.*),%s$")))
+elseif mode.value == "snippet" then
+	webapp.send_ok(mustache.render(html, view), "text/html; charset=UTF-8")
+elseif mode.value == "html" then
 	local f = io.open("www/inspector/index.html")
 	if not f then return end
 	local buf = f:read("*a")
 	f:close()
-	mg.write(string.format(buf, "-->\n", mustache.render(template, view), "\t\t<!--"))
+	webapp.send_ok(string.format(buf, "-->\n", mustache.render(html, view), "\t\t<!--"), "text/html; charset=UTF-8")
 end
